3cb112fdf74bc1d0343f89d54b454c4bd9992dd2
JudithBre <61976163+JudithBre@users.noreply.github.com>
2022-08-25 14:11:41 +0200

    Minimizing the code to the part that works

--- analyse_nc_xarray.py
+++ analyse_nc_xarray.py
@@ -1,6 +1,7 @@
+# coding=utf-8
 
 import xarray as xr
+print("xarray as xr has been imported")
 # import of a single netCDF file
 dataDIR = 'C:/Users/Judith/Documents/Studium/1 - Bachelor of Science Geoinformatik/Bachelorarbeit/BA/Daten/NC_Vector_Cube/NC/dcb.nc'
-# store in a DataSet
-DS = xr.open_dataset(dataDIR)
+print(dataDIR)
