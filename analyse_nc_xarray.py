28ca47739d84ccf05088d25abb26bba3441f0f8e
JudithBre <61976163+JudithBre@users.noreply.github.com>
2022-08-25 12:06:42 +0200

    Directory change

--- analyse_nc_xarray.py
+++ analyse_nc_xarray.py
@@ -1,6 +1,6 @@
 
 import xarray as xr
 # import of a single netCDF file
-dataDIR = 'C:/Users/Judith/Documents/Studium/1 - Bachelor of Science Geoinformatik/Bachelorarbeit/BA/Daten/NC_Edzer_Pebesma/NC/NC/dcb.nc'
+dataDIR = 'C:/Users/Judith/Documents/Studium/1 - Bachelor of Science Geoinformatik/Bachelorarbeit/BA/Daten/NC_Vector_Cube/NC/dcb.nc'
 # store in a DataSet
 DS = xr.open_dataset(dataDIR)
