608074d3814b96ba3ac18fc7e43b45bdb38680d8
JudithBre <61976163+JudithBre@users.noreply.github.com>
2022-08-25 15:45:10 +0200

    ValueError: dimension 'geometry' already exists as a scalar variable

--- analyse_nc_xarray.py
+++ analyse_nc_xarray.py
@@ -1,7 +1,13 @@
 # coding=utf-8
-
+import numpy as np
+print("numpy as np has been imported")
+import pandas as pd
+print("pandas as pd has been imported")
 import xarray as xr
 print("xarray as xr has been imported")
+
 # import of a single netCDF file
 dataDIR = 'C:/Users/Judith/Documents/Studium/1 - Bachelor of Science Geoinformatik/Bachelorarbeit/BA/Daten/NC_Vector_Cube/NC/dcb.nc'
 print(dataDIR)
+dataSet = xr.open_dataset(dataDIR)
+print(dataSet)
