52cea74723256f4d2b78d45ca8f74525335f7c39
JudithBre <61976163+JudithBre@users.noreply.github.com>
2022-08-31 09:52:36 +0200

    Update shp2netCDF.py
    Change in code

--- shp2netCDF.py
+++ shp2netCDF.py
@@ -86,32 +86,9 @@ for root, dirs, files in os.walk(shp_dir):
     print()
 
 # ----------------------------------------------------------------------------------------------------------------------
-# Test access to the attributes (optional)
-# The attributes correspond to the column headers in my shapefile.
-# ----------------------------------------------------------------------------------------------------------------------
-    print("Test access to attributes now:")
-    fID = fms_shp.findFieldMapIndex("FID")
-    print(fID)
-    shape = fms_shp.findFieldMapIndex("Shape")
-    print(shape)
-    velocity = fms_shp.findFieldMapIndex("Velocity")
-    print(velocity)
-    vprecision = fms_shp.findFieldMapIndex("Vprecision")
-    print(vprecision)
-    dEM = fms_shp.findFieldMapIndex("DEM")
-    print(dEM)
-    print("Test finished." + "\n")
-    # TODO: Why do the FID and Shape attributes have the same index?!
-
-# ----------------------------------------------------------------------------------------------------------------------
 # Loop through all attributes (columns)
 # ----------------------------------------------------------------------------------------------------------------------
-    print("Loop through all attributes (columns)")
-    print("Attribute count: ")
-    print(len(fms_shp.fields))
-    print()
-    print("Loop starts now:")
-    zaehler = 0
+    fields = ["lat", "lon"]  
     for field_idx in range(len(fms_shp.fields)):
         # Get current FieldMap
         field_map = fms_shp.getFieldMap(field_idx)
@@ -119,72 +96,19 @@ for root, dirs, files in os.walk(shp_dir):
         field_outputField = field_map.outputField
         # Get current FieldName
         field_name = field_outputField.name
-        print("Get current FieldName:")
-        print(field_name)
-        zaehler = zaehler + 1
-        print("current attribute:" + str(zaehler))
 
         # Search for date columns starting with "D_20*"
         if field_name.startswith('D_20'):
-            print(field_name + " (starts with D_20*)")
-
-            # Get FieldMap from Shapefile at current Field Index
-            fm_dH = arcpy.FieldMap()
-            fm_dH.addInputField(fms_shp, 'dH') # fms_shp.getFieldMap(field_idx)
-            fm_dH.outputField.name = 'dH'
-            fm_dH.outputField.aliasName = 'delta Height'
-            # TODO: For some reason, all values in 'dH' becoming 0
-
-            # Create new FieldMappings Object for Output Feature Class
-            fms_out = arcpy.FieldMappings()
-
-            # Fill Output Feature Class with 'lat' and 'lon' for every Date-Column, add FieldMaps to FieldMappings()
-            fms_out.addFieldMap(fm_lon)
-            fms_out.addFieldMap(fm_lat)
-#            fms_out.addFieldMap(fm_dH)
-#            print(fms_out)
-
-            # TODO: Get Date String from FieldName and modify to Datetime ('yyyy/mm/dd hh:mm:ss')
-            # TODO: Create new 'Datetime' Field
-            # TODO: Fill new Field with Datetime Value
-            '''
-            field_date = "date"
-            with arcpy.da.UpdateCursor(fms_shp, [field_date]) as rows:
-                for row in rows:
-                    rows.updateRow([datetime.date.today()])
-
-            arcpy.management.AddField(in_table, field_name, field_type, {field_precision}, {field_scale},
-                                      {field_length}, {field_alias}, {field_is_nullable}, {field_is_required},
-                                      {field_domain})
-            '''
-
-            # Prepare Output Names
-#            shp_name = shp_file.split('\\')[-1]
-#            shp_name = shp_name.split('.shp')[0]
-#            date_name = field_name.split('D_')[1]
-
-            # Set Parameter for 'FeatureClassToFeatureClass'
-#            in_features = shp_file
-#            out_path = os.path.join(root_dir, out_gdb)
-#            out_name = shp_name + "_" + date_name
-
-            # Check and Create GDB
-#            if not arcpy.Exists(os.path.join(root_dir, out_gdb)):
-#               arcpy.management.CreateFileGDB(root_dir, 'shp2netCDF.gdb', 'CURRENT')
-
-            # Check and Remove Feature Class
-#            if arcpy.Exists(os.path.join(out_path, out_name)):
-#                arcpy.Delete_management(out_name)
-
-            # Save Feature Class to GDB
-#            arcpy.conversion.FeatureClassToFeatureClass(in_features, out_path, out_name, '#', fms_out)
-
-        '''
-        fields = ["lat", "lon"] 
-        # if fieldsname starts with... "D_20" in der Schleife mit einbinden und dann fields ergänzen
-        for field_idx in range(len(fms_shp.fields)):
-        voids = arcpy.da.FeatureClassToNumPyArray(shapedateiParameter, fields)
+            fields.append(field_name)
+    
+    voids = arcpy.da.FeatureClassToNumPyArray(shapedateiParameter, fields)
+    list = []
+    for void in voids:
         
+    
+        '''
+           
+       
         # Run the Spatial Join tool, using the defaults for the join operation and join type
         arcpy.stpm.CreateSpaceTimeCube(in_features, output_cube, time_field, {template_cube}, {time_step_interval},
                                        {time_step_alignment}, {reference_time}, {distance_interval}, summary_fields,
